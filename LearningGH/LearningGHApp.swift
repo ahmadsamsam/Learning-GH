68c482cc75513a951a0c0e7187f0a1136c90e387
Ahmad Samsam <alsamsam.ahmad@gmail.com>
2021-07-31 11:05:21 +0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ahmad H.Samsam on 31/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
